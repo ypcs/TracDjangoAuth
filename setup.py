a84826aae2cf76bd313e71d154b4ae2a1f376c0e
Ville Korhonen <ville@xd.fi>
2014-10-11 03:26:24 +0300

    Fix syntax: remove invalid whitespaces.

--- setup.py
+++ setup.py
@@ -3,33 +3,29 @@
 from setuptools import setup
 
 setup(
-    name = "TracDjangoAuth",
-    version = "0.2",
-    author = "Ville Korhonen",
-    author_email = "ville@xd.fi",
-    url = "https://github.com/ypcs/TracDjangoAuth",
-    download_url = "https://github.com/ypcs/TracDjangoAuth/tarball/master",
-    description = "Trac Authentication against Django's userdb",
-    
-    keywords = [
+    name="TracDjangoAuth",
+    version="0.2",
+    author="Ville Korhonen",
+    author_email="ville@xd.fi",
+    url="https://github.com/ypcs/TracDjangoAuth",
+    download_url="https://github.com/ypcs/TracDjangoAuth/tarball/master",
+    description="Trac Authentication against Django's userdb",
+    keywords=[
         'AccountManager',
         'acct_mgr',
         'authentication',
         'Django',
         'Trac',
     ],
-    
-    packages = [
+    packages=[
         'tracdjangoauth',
     ],
-    
-    install_requires = [
+    install_requires=[
         'Trac>=0.11',
         'TracAccountManager>=0.2',
         'Django>=1.0',
     ],
-
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Framework :: Django",
@@ -41,8 +37,7 @@ setup(
         "Programming Language :: Python",
         "Topic :: System :: Systems Administration :: Authentication/Directory",
     ],
-    
-    entry_points = {
+    entry_points={
         'trac.plugins': [
             'tracdjangoauth.auth_django = tracdjangoauth.auth_django',
         ]
